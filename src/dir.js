a4a53c1a14a314277ebfb1f970b6e8cb62a00bda
hzshachaoheng <hzshachaoheng@netease.com>
2016-09-14 00:19:53 +0800

    生成目录

--- src/dir.js
+++ src/dir.js
@@ -17,3 +17,38 @@ if(!fs.existsSync('dir')){
 	console.log('当前目录已存在名为dir的文件夹');
 }
 
+
+function buildDir(dirObj, prefix){
+	if(!prefix){
+		prefix = '';
+	}
+	fs.mkdirSync(prefix + dirObj.name);
+	for(let i = 0; i < dirObj.sub.length; i++){
+		buildDir(dirObj.sub[i], prefix+dirObj.name+'/');
+	}
+}
+
+let projectDir = {
+	name: 'main',
+	sub: [{
+		name: 'mainsite',
+		sub: [{
+			name: 'index',
+			sub: []
+		},{
+			name: 'about',
+			sub: []
+		}]
+	},{
+		name: 'unicorn',
+		sub: [{
+			name: 'index',
+			sub: []
+		},{
+			name: 'about',
+			sub: []
+		}]
+	}]
+}
+buildDir(projectDir);
+
